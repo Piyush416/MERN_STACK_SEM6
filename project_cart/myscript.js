784ab644d92725fc177603edab98bb9c402a609e
Chenu <piyushbagadi51@gmail.com>
2025-01-03 13:11:15 +0530

    update the index.html to add new feature

--- project_cart/myscript.js
+++ project_cart/myscript.js
@@ -27,20 +27,59 @@ const items = [
 let cards = document.getElementById("card")
 let categorySelect = document.querySelector(".drop-content")
 let drop_btn = document.querySelector(".drop-btn")
+let boxes = document.querySelector(".items")
+let heading = document.querySelector(".heading")
+
+
+// Heading animation
+let heading_text = "MEGA MART"
+heading.textContent = ' ';
+let strlen = heading_text.length;
+let stopMyTimer
+let index = 0;
+stopMyTimer = setInterval(() => {
+    if (index < strlen) {
+        heading.textContent += heading_text.charAt(index);
+        index++;
+    }
+    else {
+        clearInterval(stopMyTimer)
+    }
+}, 300)
+
+
+// --------------------------------------------------------------------------------------------------
+// fetch the api code
+// async function fetchRandomImage(keyword) {
+//     const apiKey = 'r3BYorJ7NegRTgJ5mjKInWmAkxCh-7HiZ2bN963z7oY';
+//     const apiUrl = `https://api.unsplash.com/photos/random?query=${keyword}&client_id=${apiKey}`;
+//     try {
+//         const response = await fetch(apiUrl);
+//         if (!response.ok) throw new Error('Error fetching image');
+        
+//         const data = await response.json();
+//         const imageUrl = data.urls.regular; // Get the image URL
+//         console.log(imageUrl);
+//     } catch (error) {
+//         console.error('Error fetching the random image:', error);
+//     }
+// }
+// const imgUrl = fetchRandomImage(items[0].category)
+// --------------------------------------------------------------------------------------------------------
+
 
 
 
 // let cards = document.querySelector(".parent");
 // let categorySelect = document.getElementById("cars");
-
 let selectedCategory
-
 function myfun(selectedCategory) {
     drop_btn.textContent = selectedCategory.toUpperCase()
     items.filter(item => item.category === selectedCategory).forEach(item => {
+        // const imgUrl = fetchRandomImage(items.name);
         cards.innerHTML += `
             <div class="items">
-            <img src="https://picsum.photos/200/200">
+            <img src="https://picsum.photos/200/200" alt="Product Image">
             <h3>Product: ${item.name}</h3>
             <p>Price: ${item.price}$</p>
             <p>Category: ${item.category}</p>
@@ -51,18 +90,46 @@ function myfun(selectedCategory) {
 }
 
 
+// code for item render one by one in 300ms
+// let increment = items.map((i) => {
+//     return i;
+// })
+// index = 0;
+// let length = increment.length;
+// // let stopMyTimer
+// stopMyTimer = setInterval(() => {
+//     if (index < length) {
+//         cards.innerHTML += `
+//                 <div class="items">
+//                 <img src="https://picsum.photos/200/200" alt="Product Image">
+//                 <h3>Product: ${increment[index].name}</h3>
+//                 <p>Price: ${increment[index].price}$</p>
+//                 <p>Category: ${increment[index].category}</p>
+//                 <button>ADD ME</button>
+//                 </div>
+//             `;
+//         index++;
+//     }
+//     else {
+//         clearInterval(stopMyTimer)
+//     }
+// }, 300)
+
+
 
 items.forEach(item => {
     cards.innerHTML += `
-            <div class="items">
-            <img src="https://picsum.photos/200/200">
-            <h3>Product: ${item.name}</h3>
-            <p>Price: ${item.price}$</p>
-            <p>Category: ${item.category}</p>
-            <button>ADD ME</button>
-            </div>
-        `;
+                <div class="items">
+                <img src="https://picsum.photos/200/200" alt="Product Image">
+                <h3>Product: ${item.name}</h3>
+                <p>Price: ${item.price}$</p>
+                <p>Category: ${item.category}</p>
+                <button>ADD ME</button>
+                </div>
+            `;
 })
+
+
 
 
 categorySelect.addEventListener('click', (event) => {
